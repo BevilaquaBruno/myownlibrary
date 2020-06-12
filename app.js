9a0bb81d090f8a646d6c9bafaa43fbcd9cb06582
bevilaquabruno <bbbevilaqua@gmail.com>
2020-06-12 16:49:54 -0300

    add helmet

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+var helmet = require('helmet');
+
 var app = express();
+
+app.use(helmet());
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');

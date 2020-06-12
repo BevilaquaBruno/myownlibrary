985d5f5c79182d7f8ab1e22c1a2ca9be0c510617
bevilaquabruno <bbbevilaqua@gmail.com>
2020-06-12 17:08:48 -0300

    add body-parser and cookie-parser

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var helmet = require('helmet');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
 
-var app = express();
+var app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser())
 app.use(helmet());
  
 app.engine('handlebars', exphbs());

c984077684a354ea34df7eee926bc0e93f268078
bevilaquabruno <bbbevilaqua@gmail.com>
2020-06-24 09:35:43 -0300

    ident user

--- schemas/user.js
+++ schemas/user.js
@@ -1,23 +1,23 @@
 var mongoose = require('mongoose');
 var md5 = require('md5');
 
- var userSchema = mongoose.Schema({
-   name: { type: String },
-   email: { type: String, required: true },
-   username: { type: String, required: true },
-   password: { type: String, required: true }
- },{
-   timestamps: true
- });
+var userSchema = mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+},{
+  timestamps: true
+});
 
- userSchema.pre('save', function(next){
-   var user = this;
-   if (!user.isModified('password')) {
-     return next;
-   }
+userSchema.pre('save', function(next){
+  var user = this;
+  if (!user.isModified('password')) {
+    return next;
+  }
 
-   user.password = md5(user.password);
-   next();
- });
+  user.password = md5(user.password);
+  next();
+});
 
- module.exports = mongoose.model('User', userSchema, 'user');
+module.exports = mongoose.model('User', userSchema, 'user');

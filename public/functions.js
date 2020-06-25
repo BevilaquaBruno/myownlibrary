efd21618e406c6e9f7875d36920bc21d5865564b
bevilaquabruno <bbbevilaqua@gmail.com>
2020-06-25 16:56:23 -0300

    turn 2 functions public

--- public/functions.js
+++ public/functions.js
@@ -3,3 +3,45 @@ function formatDateInput(f, fVal) {
   if (f.value != '')
     document.getElementById(fVal).value = moment(f.value).format('YYYY-MM-DD');
 }
+
+async function getLanguagesSelect(old, current) {
+  let response;
+  try {
+    response = await axios.get('/idioma/todos');
+  } catch (error) {
+    response = {};
+    console.error(error);
+  }
+  let languages = response.data.languages;
+  let languages_select = document.getElementById(current);
+  languages.forEach(el => {
+    let option = document.createElement("option");
+    option.text = el.description;
+    option.value = el._id;
+    if (el._id == document.getElementById(old).value ) {
+      option.selected = "selected";
+    }
+    languages_select.appendChild(option);
+  });
+}
+
+async function getCountriesSelect(old, current) {
+  let response;
+  try {
+    response = await axios.get('/pais/todos');
+  } catch (error) {
+    response = {};
+    console.error(error);
+  }
+  let countries = response.data.countries;
+  let countries_select = document.getElementById(current);
+  countries.forEach(el => {
+    let option = document.createElement("option");
+    option.text = el.name;
+    option.value = el._id;
+    if (el._id == document.getElementById(old).value ) {
+      option.selected = "selected";
+    }
+    countries_select.appendChild(option);
+  });
+}
